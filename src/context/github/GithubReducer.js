170d8fdb7a093f03957826ed28cc2f06891376af
Saurav <skalita22.dev@gmail.com>
2022-10-31 20:39:23 +0530

    Updated with axios and refactoring

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -4,19 +4,14 @@ const githubReducer = (state, action) => {
             return {
                 ...state,
                 users: action.payload,
-                loading : false
+                loading: false
             }
-        case 'GET_USER':
+        case 'GET_USER_AND_REPOS':
             return {
                 ...state,
-                user: action.payload,
-                loading : false
-            }
-        case 'GET_REPOS':
-            return {
-                ...state,
-                repos: action.payload,
-                loading : false
+                user: action.payload.user,
+                repos: action.payload.repos,
+                loading: false
             }
         case 'SET_LOADING':
             return {
